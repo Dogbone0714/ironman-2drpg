656bc3e815165ecb802f0f9f92cb30a7ee0a3df0
Dogbone0714 <jameskang0714@gmail.com>
2021-01-05 09:59:55 +0800

    1

--- Headers/SystemSetting.h
+++ Headers/SystemSetting.h
@@ -4,7 +4,13 @@
 #define __SYSTEMSETTING_H__
 
 #include <stdio.h>
-#include <string.h>  // 等等會用到字串操作，預先引入
+#include <string.h>  
 #include <windows.h>
+
+HANDLE handler;  // 控制視窗的變數，視為視窗的控制把手
+COORD xyPoint;  // Windows用的x, y座標型態
+CONSOLE_CURSOR_INFO cursorInfo;  // 有關光標的資訊構成的結構
+
+void selfCls();  // 自行撰寫的清空螢幕
 
 #endif
