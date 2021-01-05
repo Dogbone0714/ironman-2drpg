3ca3580234f0500380afc1d5c10122da91552266
Dogbone0714 <jameskang0714@gmail.com>
2021-01-05 10:00:18 +0800

    1

--- Sources/SystemSetting.c
+++ Sources/SystemSetting.c
@@ -1,3 +1,15 @@
 /* File: SystemSetting.c */
 
 #include "SystemSetting.h"
+
+void selfCls()  // 此清空螢幕原理是直接將光標帶到第一個位置，再直接更新畫面
+{
+    handler = GetStdHandle(STD_OUTPUT_HANDLE);  // 取得標準輸出的handle
+    xyPoint.X = 0;  // 希望x, y座標都能到0的位置
+    xyPoint.Y = 0;
+    SetConsoleCursorPosition(handler, xyPoint);  // 將座標設置到該位置
+    cursorInfo.bVisible = FALSE;  // 光標設為不可見
+    cursorInfo.dwSize = 1;  // 光標大小用不到，先設為1
+    SetConsoleCursorInfo(handler, &cursorInfo);  // 設置光標資訊
+}
+
